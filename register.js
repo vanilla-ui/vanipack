5a903ebe763a1d790aba75b7f193d702b650e257
IntPtr <lxy.lixiaoyan@gmail.com>
2017-02-20 16:32:07 +0800

    update babel-register ignore rules

--- register.js
+++ register.js
@@ -1,5 +1,8 @@
 require("babel-register")({
-  ignore: /node_modules\/(?!vanilla)/,
+  ignore: filename => (
+    /node_modules\/(?!vanilla)/.test(filename) ||
+    /\.vanilla/.test(filename)
+  ),
 
   plugins: [
     "transform-es2015-modules-commonjs",
